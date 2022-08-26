0dbda2a23a4e1830d7a74d145eafff084c2d2774
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-25 18:00:38 -0600

    add completed books assignment

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns){
+  return{
+     name: name,
+     age: age,
+     pronouns: pronouns
+   }
+}
+function saveReview(statement, listOfReviews){
+  if (listOfReviews.includes(statement) === false){
+    return listOfReviews.push(statement)
+  }
+}
+
+function calculatePageCount(title){
+  return title.length * 20
+}
+
+function writeBook(bookTitle, bookCharacter, genre){
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookTitle.length * 20,
+    genre: genre
+  }
+}
+
+function editBook(book){
+  book.pageCount = book.pageCount * .75
+  return book
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
