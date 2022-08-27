595c700e83420bd1b0658c3e62b6c2bab9f2a4ea
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-27 17:21:44 -0600

    add the shelf javascript file.

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(title, shelf){
+  if (shelf.length < 3){
+  return shelf.unshift(title)
+  }
+}
+
+function unshelfBook(bookTitle, sciFiShelf){
+  for (var i = 0; i < sciFiShelf.length; i++){
+    if (sciFiShelf[i].title === bookTitle){
+      sciFiShelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(fantasyShelf){
+  for (var i = 0; i < fantasyShelf.length; i++){
+    return `${fantasyShelf[0].title}, ${fantasyShelf[1].title}, ${fantasyShelf[2].title}`
+  }
+}
+
+function searchShelf(sciFiShelf, bookTitle){
+  if (!sciFiShelf.length) return false
+  for (var i = 0; i < sciFiShelf.length; i++){
+    if (sciFiShelf[i].title === bookTitle){
+      //if we ever find the title then we return true
+      return true
+    }
+  }
+  //if we looped through the entire array and didn't find the title then we want to retutrn false
+  return false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+   unshelfBook,
+   listTitles,
+   searchShelf
 };
